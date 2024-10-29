c5d66f70f61b187eba0c6f310c02a42b2bd9a45b
Faiyan khan <144541895+faiyankhan@users.noreply.github.com>
2024-10-29 17:43:22 +0000

    Const created

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const images = ["images/one.jpg", "images/Two.jpg", "images/Three.jpg", "images/Four.jpg", "images/Five.jpg"];
+
+let index = 0;
+const slide = document.getElementById('slide');
+
 function next() {
-    const slide = document.getElementById('slide');
+    index++;
+
+    slide.src = images[index];
 }
